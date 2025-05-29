ebf3959cbebedb02bc4f9075864244c6538f01c8
Mshahwaz <shahwazkh47@gmail.com>
2025-05-30 00:37:35 +0530

    final Commit i think

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,6 +1,8 @@
-for i in 0..500
-  describe "These are sample RSpec tests" do
+describe "These are sample RSpec tests" do
+  (0..500).each do |i|
     it "This is test ##{i}" do
+      # test code here
     end
   end
 end
+
